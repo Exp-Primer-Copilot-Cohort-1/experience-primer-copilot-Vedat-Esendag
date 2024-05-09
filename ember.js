d39713ae0b906c22934a288a2367875f64a5e6f0
Vedat-Esendag <91906898+Vedat-Esendag@users.noreply.github.com>
2024-05-09 22:52:03 +0200

    Copilot second commit

---
+++ ember.js
@@ -0,0 +1,15 @@
+function skillsMember() {
+    var skills = {
+        "frontend": ["HTML", "CSS", "JavaScript", "jQuery", "AngularJS", "Ember.js", "ReactJS", "Bootstrap"],
+        "backend": ["NodeJS", "Express", "Ruby", "Rails", "PHP", "Laravel", "Java", "Spring", "Python", "Django"],
+        "database": ["MySQL", "PostgreSQL", "MongoDB", "SQLServer", "Oracle"],
+        "devops": ["Git", "Docker", "Jenkins", "Maven", "Gradle", "Nexus", "Artifactory", "Terraform", "Ansible"],
+        "cloud": ["AWS", "Azure", "Google Cloud", "IBM Cloud", "Oracle Cloud", "Alibaba Cloud", "Digital Ocean"],
+        "mobile": ["Android", "iOS", "Xamarin", "React Native", "PhoneGap", "Ionic", "Cordova"],
+        "testing": ["JUnit", "TestNG", "Jasmine", "Karma", "Mocha", "Chai", "Selenium", "Cypress", "Appium"],
+        "agile": ["Scrum", "Kanban", "XP", "Lean", "SAFe", "TDD", "BDD", "ATDD", "CI/CD"],
+        "tools": ["JIRA", "Confluence", "Slack", "Trello", "Asana", "Basecamp", "MS Project", "Gantt Chart"]
+    };
+
+    return skills;
+}
